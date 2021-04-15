2b7dde4fd7f9ff97e1ecd91c639073fa5415948f
lbc19920615 <1585638808@qq.com>
2021-04-15 10:39:47 +0800

    Create variable.ts

---
+++ src/utils/variable.ts
@@ -0,0 +1,14 @@
+interface ElOption {
+    label: string,
+    value: any
+}
+
+export const options : Array<ElOption> = [
+    {
+        label: 'sdsds',
+        value: 1
+    }
+]
+
+export type WindowStates = "open" | "closed" | "minimized";
+
