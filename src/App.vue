9bc95711742dbe4e300b5f736f7c6c3fa2c9a0d3
baichao ling <1585638808@qq.com>
2021-07-08 14:58:44 +0800

    优化代码 添加suspense支持

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+<!--    <router-view></router-view>-->
+
+    <router-view v-slot="{ Component }">
+      <template v-if="Component">
+        <transition mode="out-in">
+          <keep-alive>
+            <suspense>
+              <component :is="Component"></component>
+              <template #fallback>
+                <app-loading></app-loading>
+              </template>
+            </suspense>
+          </keep-alive>
+        </transition>
+      </template>
+    </router-view>
+
   </div>
 </template>
 
