db46d43efe472103edd9aa42a948f7ff9a31c963
凌柏超 <1585638808@qq.com>
2021-07-11 14:31:22 +0800

    添加代码demo

--- src/plugins/highlight.js
+++ src/plugins/highlight.js
@@ -1,4 +1,4 @@
-import VueHighlightJS from 'vue-highlightjs'
+import VueHighlightJS from 'vue3-highlightjs'
 import 'highlight.js/styles/vs2015.css'
 export function install(app) {
   app.use(VueHighlightJS)
