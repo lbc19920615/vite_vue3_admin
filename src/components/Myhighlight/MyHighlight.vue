afe39309b337123af78c38092780fb158a21b576
baichao ling <1585638808@qq.com>
2021-07-12 15:42:03 +0800

    实现代码

--- src/components/Myhighlight/MyHighlight.vue
+++ src/components/Myhighlight/MyHighlight.vue
@@ -7,30 +7,52 @@ $sel: ".my-highlight";
 </style>
 
 <template>
-  <pre v-highlightjs class="my-highlight">
+<div>
+  <slot></slot>
+  <pre v-if="rendered" v-highlightjs class="my-highlight">
 <code
-  class="hljs"
-  :class="computeCls"
-  v-html="computeContent"
+    class="hljs"
+    :class="computeCls"
+    v-html="computeContent"
 ></code>
 </pre>
+</div>
 </template>
 
 <script>
+import escape from "lodash/escape";
+import pretty from 'pretty'
+/**
+ *
+ * @param lang
+ * @param content
+ * @returns {{cls: string[], lang, content}}
+ */
+export function registerLanguage(lang, content) {
+  return {
+    cls: ['language-' + lang],
+    lang: lang,
+    content: escape(content)
+  }
+}
+
 export default {
   name: 'MyHighlight',
   props: {
-    language: {
-      type: String,
-      default: ''
-    },
-    content: {
-      type: String,
-      default: ''
-    }
+    // language: {
+    //   type: String,
+    //   default: ''
+    // },
+    // content: {
+    //   type: String,
+    //   default: ''
+    // }
+    tpl: String
   },
   data() {
-    return {}
+    return {
+      rendered: false
+    }
   },
   computed: {
     computeCls() {
@@ -40,6 +62,22 @@ export default {
     },
     computeContent() {
       return this.content
+    }
+  },
+  created() {
+    let template = document.getElementById(this.tpl)
+    if (template) {
+      let lang = template.getAttribute('lang')
+      let html = pretty(template.innerHTML.trim())
+      this.register(lang, html)
+    }
+  },
+  methods: {
+    register(lang, content) {
+      let obj = registerLanguage(lang, content)
+      this.language = obj.lang
+      this.content = obj.content
+      this.rendered = true
     }
   }
 }
--- src/views/home/search.vue
+++ src/views/home/search.vue
@@ -8,38 +8,19 @@ $sel: "." + $tag;
 
 <template>
   <div class="pgae-search">
-    <my-highlight
-        :language="highlights[0].lang"
-        :content="highlights[0].content"
-    ></my-highlight>
+    <my-highlight tpl="search-doc-1">
+    </my-highlight>
   </div>
 </template>
 
 <script>
-import escape from 'lodash/escape'
 import MyHighlight from "@/components/Myhighlight/MyHighlight.vue";
-
-export function registerLanguage(lang, content) {
-  return {
-    cls: ['language-' + lang],
-    lang: lang,
-    content: escape(content)
-  }
-}
 
 export default {
   name: "Search",
   components: {MyHighlight},
-  data() {
-    return {
-      highlights: [
-        registerLanguage('html', `<RemoteOptions
-:request-method="myRequestMethod"
-label-key="name"
-value-key="id"
-></RemoteOptions>`)
-      ]
-    }
+  mounted() {
+
   }
 }
 </script>
