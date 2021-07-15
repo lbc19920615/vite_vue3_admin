3e8eff4070da32694227b7886ff214263b5d7a35
baichao ling <1585638808@qq.com>
2021-07-15 09:33:15 +0800

    使用twigvue格式来初始化代码

--- src/plugins/remote.js
+++ src/plugins/remote.js
@@ -8,56 +8,58 @@ export function install (app) {
     }
   })
 
-  import('@my-virtual-file:src=template-field.twig').then(res => {
-    let templateId = 'cm-field-tpl'
-
-    globalThis.initTemplate(templateId, globalThis, {
-      html: res.default
-    })
-
-    app.component('cm-field', {
-      template: '#' + templateId,
-      props: {
-        prop: String,
-        label: String,
-        type: String,
-        formPath: String,
-        modelValue: null,
-        ui: {
-          type: Object,
-          default() {
-            return {}
-          }
-        }
-      },
-      setup(props, {emit}) {
-        const { ref, onMounted } = Vue
-        // console.log(props.modelValue)
-        onMounted(() => {
-          value.value = props.modelValue
-        })
-
-        const value = ref(null)
-        function onInput(e) {
-          // console.log('onInput', props.modelValue, e)
-          // console.log('value.value', value.value)
-          emit('update:modelValue', value.value)
-        }
-        function onChange(e) {
-          emit('update:modelValue', value.value)
-        }
-        return {
-          onInput,
-          value,
-          onChange,
-        }
-      }
-    })
+  import("__remote/getscript?src=cmField/index.twigvue").then((def) => {
+    if (def.default) {
+      const com = def.default('CmField')
+      // console.log(com)
+      app.component(com.name, com)
+    }
   })
 
-  // globalThis.loadTemplate('cm-field-tpl', globalThis, {
-  //   path: 'template-field'
-  // }).then(({html}) => {
-  //   // console.log('html', html)
+  // import('@my-virtual-file:src=template-field.twig').then(res => {
+  //   let templateId = 'cm-field-tpl'
+  //
+  //   globalThis.initTemplate(templateId, globalThis, {
+  //     html: res.default
+  //   })
+  //
+  //   app.component('cm-field', {
+  //     template: '#' + templateId,
+  //     props: {
+  //       prop: String,
+  //       label: String,
+  //       type: String,
+  //       formPath: String,
+  //       modelValue: null,
+  //       ui: {
+  //         type: Object,
+  //         default() {
+  //           return {}
+  //         }
+  //       }
+  //     },
+  //     setup(props, {emit}) {
+  //       const { ref, onMounted } = Vue
+  //       // console.log(props.modelValue)
+  //       onMounted(() => {
+  //         value.value = props.modelValue
+  //       })
+  //
+  //       const value = ref(null)
+  //       function onInput(e) {
+  //         // console.log('onInput', props.modelValue, e)
+  //         // console.log('value.value', value.value)
+  //         emit('update:modelValue', value.value)
+  //       }
+  //       function onChange(e) {
+  //         emit('update:modelValue', value.value)
+  //       }
+  //       return {
+  //         onInput,
+  //         value,
+  //         onChange,
+  //       }
+  //     }
+  //   })
   // })
 }
