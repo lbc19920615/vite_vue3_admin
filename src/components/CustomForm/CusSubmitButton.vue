05cddec7b439d336522da5fe6b68d127c3a10175
baichao ling <1585638808@qq.com>
2021-08-20 08:58:50 +0800

    支持了防止误触button

--- src/components/CustomForm/CusSubmitButton.vue
+++ src/components/CustomForm/CusSubmitButton.vue
@@ -1,12 +1,15 @@
 <template>
-  <el-button type="primary" @click="submit"><slot>提交</slot></el-button>
+<!--  <el-button type="primary" @click="submit"><slot>提交</slot></el-button>-->
+  <SubmitButton type="primary"  @submit="submit"><slot>提交</slot></SubmitButton>
 </template>
 
 <script>
 import {RefsManagerMixin, slotComEventMixin} from "@/plugins/form-render/mixins";
+import SubmitButton from "@/components/SubmitButton.vue";
 
 export default {
   name: "CusSubmitButton",
+  components: {SubmitButton},
   mixins: [
     RefsManagerMixin({
     }),
