63e2533fefd27702d19d937539315df3cc23b991
baichao ling <1585638808@qq.com>
2021-08-23 10:07:01 +0800

    Update DeepPropEditor.vue
    完成prop无限极

--- src/views/about/components/DeepPropEditor.vue
+++ src/views/about/components/DeepPropEditor.vue
@@ -92,7 +92,11 @@ let plumbLayoutMixin = {
               id: 'i111-0',
               key: 'parts',
               data: '',
-              root: true
+            },
+            {
+              id: 'i111-1',
+              key: 'other',
+              data: '',
             },
           ],
         },
@@ -176,55 +180,146 @@ let plumbLayoutMixin = {
       }
     },
     onSaveDep({deps, links = []}) {
+      const JSON_PARSE_KEYS = ['widgetConfig', 'rules']
+
+      function findItemLinkedDep(item) {
+        let dep
+        let link = links.find(v => v.from === item.id)
+        if (link) {
+          dep = deps.find(v => v.id === link.toPID)
+        }
+        return dep
+      }
+
       // console.log('onGetData', deps, links)
       let deepObj = {}
       let cur =  deps.find(v => v.id === this.rootId)
 
-      function handleDeep(cur, deepPoint) {
-        if (cur.type === 'array') {
-          // console.log('array')
+      let setObj = ZY.lodash.set
+      let JSON5 =  ZY.JSON5
 
+      function deepTransform(target, obj, path) {
 
-        }
-        if (cur.type === 'object') {
-          deepPoint = {
-            type: 'object',
-            properties: {
-            }
-          }
-          for (let item of cur.items) {
-            let parsedData = {}
-            try {
-              parsedData = ZY.JSON5.parse(item.data)
-            } catch (e) {
-              //
-            }
-            // if (item.root) {
+        for (let [key, value] of Object.entries(obj)) {
+          if (Array.isArray(value)) {
             //
-            // } else {
-            //   let keys = Object.keys(parsedData)
-            //   if (keys.length > 0) {
-            //
-            //   }
-            //   keys.forEach(key => {
-            //     deepPoint[key] = parsedData[key]
-            //   })
-            // }
-
-            // console.log(links)
-            console.log(deepPoint)
-            // let link = links.find(v => v.from === item.id)
-            // if (link) {
-            //   let dep = deps.find(v => v.id === link.toPID)
-            //   if (dep) {
-            //     handleDeep(dep, deepPoint.properties)
-            //   }
-            // }
+          } else {
+            // 当value是object类型 递归查找
+            if (typeof value === 'object') {
+              deepTransform(target, value, `${path}['${key}']`)
+            } else {
+              // 只有特殊key才被翻译
+              if (JSON_PARSE_KEYS.includes(key)) {
+                // context.obj[key] = JSON5.parse(value)
+                // setObj(target, path, JSON5.parse(value))
+                // console.log(key, path)
+                let parent = ZY.lodash.get(target, path)
+                if (path === '') {
+                  parent = target
+                }
+                // console.log(parent, key, value)
+                try {
+                  parent[key] = JSON5.parse(value)
+                } catch (e) {
+                  parent[key] = {}
+                }
+              }
+            }
           }
         }
       }
 
-      handleDeep(cur, deepObj)
+      function parseEditorData(dataStr) {
+        try {
+          let obj = JSON5.parse(dataStr)
+          deepTransform(
+              obj,
+              obj,
+              ''
+          )
+          // console.log(obj)
+          return obj
+        } catch (e) {
+          return {}
+        }
+      }
+
+      function handleDeep(curDep = {}, path) {
+        if (curDep.type === 'array') {
+          // console.log('array')
+          setObj(deepObj, path, {
+            type: 'array',
+            items: {
+            }
+          })
+
+          let newPath = `${path}['items']`
+
+          // console.log(deepObj)
+          // 因为array是特殊的object 只有items一个属性
+          let item = curDep.items[0]
+          let dep = findItemLinkedDep(item)
+          if (dep) {
+            handleDeep(dep, newPath)
+          }
+        }
+        else if (curDep.type === 'object') {
+          setObj(deepObj, path,
+              {
+                type: 'object',
+                properties: {
+                },
+              }
+          )
+
+          for (let item of curDep.items) {
+            let parsedData = {}
+            parsedData = parseEditorData(item.data)
+            let newPath = `${path}['properties']['${item.key}']`
+
+            // deepPoint.properties[item.key] = parsedData
+
+            console.log(path, parsedData)
+
+            setObj(deepObj, newPath,
+                parsedData
+            )
+
+            // 找寻当前item是否有绑定的下一个dep
+            let dep = findItemLinkedDep(item)
+
+            // console.log(item, dep, deepObj, newPath)
+            if (dep) {
+              handleDeep(dep, newPath)
+            }
+          }
+        }
+        else {
+          console.log('not array and object', curDep, deepPoint)
+        }
+      }
+
+      `
+{
+  type: 'object',
+  properties: {
+    parts: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name1: {},
+          name2: {},
+        },
+      },
+    },
+  },
+}
+      `
+
+      handleDeep(cur, 'root')
+
+      console.log(ZY.JSON5.stringify(deepObj.root.properties, null, 2))
 
       let map = {
         [this.rootId]: deps.find(v => v.id === this.rootId)
