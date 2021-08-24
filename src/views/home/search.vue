2fcab27e69db7f63b70dcd20ccbd9068da9b6989
baichao ling <1585638808@qq.com>
2021-08-24 16:55:37 +0800

    支持swagger直接变成代码

--- src/views/home/search.vue
+++ src/views/home/search.vue
@@ -3,15 +3,20 @@
   <div v-if="page.inited">
 <!--    <div>{{store.model}}</div>-->
     <div>{{store.computedModel}}</div>
-    <template v-if="page.val('options')">
-      <EwSelect v-model="store.model.selectedValue" :options="store.model.options"></EwSelect>
-    </template>
 
     <el-input v-model="store.model.swaggerOrigin" placeholder="placeholder"></el-input>
     <submit-button @submit="onSubmit">开始分析</submit-button>
 
-    <div>
+    <div v-if="page.val('options')">
+      <EwSelect v-model="store.model.selectedValue" :options="store.model.options"></EwSelect>
+    </div>
+
+    <div v-if="page.val('currentSwagger')">
       <textarea style="width: 100%; height: 30vh" v-model="store.model.currentSwagger"></textarea>
+    </div>
+
+    <div v-for="file in store.model.files">
+      <a :href="file" target="_blank" download>{{file}}</a>
     </div>
   </div>
 </template>
@@ -21,11 +26,14 @@ import EwSelect from "@/components/Ew/EwSelect.vue"
 import {onMounted, reactive, ref} from "vue";
 import {useControl} from "@/mixins/framework";
 
+let SEVER_ORIGIN = `http://192.168.1.67:7001/`
+
 function onInited({storeControl}) {
   storeControl.set({
     haha: 111,
     swaggerOrigin: 'http://192.168.1.60:7888',
-    selectedValue: ''
+    selectedValue: '',
+    files: [],
   })
   // console.log('sdsdsds', storeControl.store)
 }
@@ -53,6 +61,9 @@ let properties = {
   },
   currentSwagger: {
     type: String
+  },
+  files: {
+    type: Array,
   }
 }
 let computed = {
@@ -67,12 +78,20 @@ let computedChange = {
   async ['selectedOption'](newVal) {
     console.log('newVal', newVal)
     if (newVal) {
+      let jsonOrigin = `${page.val('swaggerOrigin')}${newVal.value}`
       let [,{data}] = await ZY.awaitTo(
-          globalThis.Req.get(`${page.val('swaggerOrigin')}${newVal.value}`)
+          globalThis.Req.get(jsonOrigin)
       )
       page.setByPath('currentSwagger', JSON.stringify(data, null, 2))
 
-      console.log(JSON.stringify(data, null, 2))
+      // console.log(JSON.stringify(data, null, 2))
+      // http://192.168.1.67:7001/?url=http://192.168.1.60:7888/api-system/v2/api-docs&folder=folder3
+      let folder = 'folder-' + newVal.label
+      let [,res] = await ZY.awaitTo(
+        globalThis.Req.get(`${SEVER_ORIGIN}?url=${jsonOrigin}&folder=${folder}`)
+      )
+      console.log('res', res)
+      page.setByPath('files', res.data.files)
     }
   }
 }
