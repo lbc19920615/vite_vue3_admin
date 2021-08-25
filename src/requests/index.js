5614c9ebfd1a1405399f9c65f4cefecf6930a82b
baichao ling <1585638808@qq.com>
2021-08-25 15:18:11 +0800

    Update index.js

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,10 +1,24 @@
-import { request } from '@/plugins/z-request/index.js'
+import { request, context } from '@/plugins/z-request/index.js'
 import { serviceOptions } from '@/requests/services/serviceOptions.js'
 serviceOptions.axios = request
 
-import { ProductsTestService } from "@/requests/services";
+import {ElMessage} from "element-plus";
+context.Message = ElMessage
 
-// console.log(ProductsTestService)
+let services = {}
+import("@/requests/services").then(res => {
+  services['auth'] = res
+  console.log(services)
+})
+let API = {}
+API.call = function (path, ...args) {
+  if (ZY.lodash.has(services, path)) {
+    let fun = ZY.lodash.get(services, path)
+    return fun(...args);
+  }
+  return Promise.reject(new Error('no service'))
+}
+globalThis.API = API
 
 // ProductsTestService.productsTest()
 
