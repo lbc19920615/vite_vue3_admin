9ea1cf20127d7a2e03f46432820e6c09c660d174
baichao ling <1585638808@qq.com>
2021-08-27 10:42:36 +0800

    remove useless

--- src/utils/event-types.js
+++
@@ -1,4 +0,0 @@
-export const SYSTEM_CONSOLE_LOG = 'console_log'
-
-
-export const FETCH_COMPONENT_READY = 'fetch_component_ready'
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,12 +1,8 @@
-import PubSub from 'pubsub-js'
-import {SYSTEM_CONSOLE_LOG} from "./event-types";
+
 
 export function log(...args) {
-    return PubSub.publish.apply(PubSub, [SYSTEM_CONSOLE_LOG, ...args])
+    return console.log(...args)
+    // return PubSub.publish.apply(PubSub, [SYSTEM_CONSOLE_LOG, ...args])
 }
 
-export function setupLoggerHandler() {
-    PubSub.subscribe(SYSTEM_CONSOLE_LOG, function (msg, ...args) {
-        console.log(...args)
-    })
-}
+
