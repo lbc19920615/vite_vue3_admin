5e06bba870de7759068e2b5ecc23145fbc64e9a8
baichao ling <1585638808@qq.com>
2021-09-08 17:23:58 +0800

    Update demo.vue

--- src/views/home/demo.vue
+++ src/views/home/demo.vue
@@ -1,34 +1,37 @@
 <template>
-  <iframe ref="iframe" src="http://192.168.1.67:8080/#/main" style="width: 100%; height: 600px;" frameborder="0"></iframe>
+  <iframe ref="iframe"  src="http://192.168.1.67:8080/#/main" style="width: 100%; height: 600px;" frameborder="0"></iframe>
 </template>
 
 <script>
-class Message {
-  static Message = 1
-  type = 'Message'
-  msg = ''
-  constructor(msg) {
-    this.msg = msg
-  }
-}
 
 export default {
   mounted() {
-    let channel = new MessageChannel();
+    import('__remote/public/message.js').then(Lib => {
+      let channel = new MessageChannel();
 
-    let ifr = this.$refs.iframe
-    let otherWindow = ifr.contentWindow;
+      let ifr = this.$refs.iframe
+      let ifrWindow = ifr.contentWindow;
 
-    ifr.addEventListener("load", iframeLoaded, false);
+      ifr.addEventListener("load", iframeLoaded, false);
 
-    function iframeLoaded() {
-      otherWindow.postMessage(new Message('Hello sdsdsdsds'), '*', [channel.port2]);
-    }
+      function sendMessage(obj) {
+        ifrWindow.postMessage(obj, '*', [channel.port2]);
+      }
 
-    channel.port1.onmessage = handleMessage;
-    function handleMessage(e) {
+      function iframeLoaded() {
+        sendMessage(new Lib.CommandMessage('open', [1,2,3]));
+      }
 
-    }
+      channel.port1.onmessage = handleMessage;
+      function handleMessage(e) {
+        let data = e.data
+        let isCustom = Lib.detectIsCustomMessage(data)
+        if (isCustom) {
+          let {name, value} = data.msg
+          console.log('sdsdsdsdsd', name, value)
+        }
+      }
+    })
   }
 }
 </script>
