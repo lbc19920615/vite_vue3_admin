02e937176c76ab9dc17bbfb81b362fddffd8517b
baichao ling <1585638808@qq.com>
2021-09-09 17:25:38 +0800

    Update index.js

--- src/components/WebElements/index.js
+++ src/components/WebElements/index.js
@@ -1,4 +1,4 @@
 export { default as MyWebDialog } from './dialog'
-export { default as MyWebTab } from './tab'
-export { default as MyWebTabPanel } from './tab-panel'
+// export { default as MyWebTab } from './tab'
+// export { default as MyWebTabPanel } from './tab-panel'
 export { default as MyFixed } from './fixed'
