870a57552c6675cea5a0ba13fb9fd5353ff03138
baichao ling <1585638808@qq.com>
2021-10-15 15:53:26 +0800

    Update CusDateTimePicker.vue

--- src/components/CustomForm/CusDateTimePicker.vue
+++ src/components/CustomForm/CusDateTimePicker.vue
@@ -1,18 +1,29 @@
 <template>
-  <el-date-picker v-model="state.value"
-                  v-bind="widgetConfig"
-                  @change="methods.onChange"
-  ></el-date-picker>
+  <template v-if="inited">
+    <el-date-picker v-model="state.value"
+                    type="datetime"
+                    v-bind="widgetConfig"
+                    @change="methods.onChange"
+    ></el-date-picker>
+  </template>
 </template>
 
 <script>
-import {CustomRenderControlMixin, defineCustomRender} from "../../plugins/form-render/utils";
+import {CustomRenderControlMixin, defineCustomRender} from "@/plugins/form-render/utils";
+import {createBaseCusCONFIG} from "@/plugins/z-frame/CusBaseEditor";
 
 export default {
   name: 'CusDateTimePicker',
   mixins: [
       CustomRenderControlMixin
   ],
+  CUS_EDITOR: function () {
+    return {
+      props: {
+        ...createBaseCusCONFIG()
+      }
+    }
+  },
   setup(props, ctx) {
     let { data, methods } = defineCustomRender(props, ctx)
     let state = data()
