d7bf2eafd295e3ccd71cda89713d2b60d8932fc3
baichao ling <1585638808@qq.com>
2021-10-28 14:19:24 +0800

    Update ZOptionsManager.vue

--- src/plugins/z-frame/components/ZOptionsManager.vue
+++ src/plugins/z-frame/components/ZOptionsManager.vue
@@ -20,13 +20,17 @@
    <template #button-content>枚举值管理</template>
    <div class="z-opt-man-group">
 <!--         {{state}}-->
-     <div class="z-opt-man-group__header a-space-mb-10">
+     <el-row align="middle"  class="z-opt-man-group__header a-space-mb-10">
        <!--      <el-button type="primary" @click="loadFile">加载</el-button>-->
        <el-button size="small" type="primary" @click="save">保存</el-button>
        <el-button size="small" type="primary" @click="exportFile">导出到本地</el-button>
-     </div>
+       <el-space class="a-space-ml-10">
+         <div>过滤name</div>
+         <el-input v-model="state.search"></el-input>
+       </el-space>
+     </el-row>
      <el-scrollbar height="50vh" class="a-space-mb-10">
-       <div class="z-opt-man-item a-space-mb-10" v-for="option in state.options">
+       <div class="z-opt-man-item a-space-mb-10" v-for="option in filterOptions(state.options)">
          <!--      {{option}}-->
          <el-row class="z-opt-man-item__label a-space-mb-10">
            <z-cell-item label="name">
@@ -214,12 +218,26 @@ export default {
       )
     }
 
+    function filterOptions(arr = []) {
+      let search = state.search
+      if (!search) {
+        return arr
+      }
+      return arr.filter(v => {
+        if (!v.name) {
+          return true
+        }
+        return v.name.includes(search)
+      })
+    }
+
     return {
       appendOption,
       save,
       appendGroup,
       deleteItem,
       exportFile,
+      filterOptions,
       state,
       onOpened
     }
