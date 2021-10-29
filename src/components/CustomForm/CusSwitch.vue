4dc15fcf44dbdac5e5f41e810ffeaf85d2246d9a
baichao ling <1585638808@qq.com>
2021-10-29 10:52:19 +0800

    fix

--- src/components/CustomForm/CusSwitch.vue
+++ src/components/CustomForm/CusSwitch.vue
@@ -19,6 +19,7 @@ export default {
   mixins: [
     CustomRenderControlMixin
   ],
+  CUS_TITLE: '选择',
   CUS_EDITOR: function () {
     return {
       props: {
--- src/plugins/z-frame/components/CusUpload.vue
+++ src/plugins/z-frame/components/CusUpload.vue
@@ -16,6 +16,7 @@ export default {
   mixins: [
       CustomRenderControlMixin
   ],
+  CUS_TITLE: '上传',
   CUS_EDITOR: function () {
     return {
       props: {
