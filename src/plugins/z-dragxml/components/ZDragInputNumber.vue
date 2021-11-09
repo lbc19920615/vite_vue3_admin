94bb154c45fccd367bee481c1d2f53202dffb3df
baichao ling <1585638808@qq.com>
2021-11-09 15:32:01 +0800

    Update ZDragInputNumber.vue

--- src/plugins/z-dragxml/components/ZDragInputNumber.vue
+++ src/plugins/z-dragxml/components/ZDragInputNumber.vue
@@ -27,8 +27,9 @@ export default {
     }
   },
   DRAG_LABEL_XML() {
-    return `<div class="z-dragxml-row"><svg t="1636083425081" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2423" width="16" height="16"><path d="M118.784 727.04h778.24V296.96h-778.24v430.08zM77.824 256h860.16v512H77.824V256zM194.56 409.6v204.8c0 12.288 8.192 20.48 20.48 20.48s20.48-8.192 20.48-20.48V409.6c0-12.288-8.192-20.48-20.48-20.48s-20.48 8.192-20.48 20.48z" p-id="2424"></path></svg>
-<div class="z-dragxml-label__name">单行输入框</div>
+    return `<div class="z-dragxml-row">
+<svg t="1636083425081" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2423" width="16" height="16"><path d="M118.784 727.04h778.24V296.96h-778.24v430.08zM77.824 256h860.16v512H77.824V256zM194.56 409.6v204.8c0 12.288 8.192 20.48 20.48 20.48s20.48-8.192 20.48-20.48V409.6c0-12.288-8.192-20.48-20.48-20.48s-20.48 8.192-20.48 20.48z" p-id="2424"></path></svg>
+<div class="z-dragxml-label__name">数字输入框</div>
 </div>`
   },
   mixins: [
