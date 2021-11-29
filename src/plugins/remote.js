accc51c55f504df3934f11f6863b43e92e216733
baichao ling <1585638808@qq.com>
2021-11-29 15:41:38 +0800

    Update remote.js

--- src/plugins/remote.js
+++ src/plugins/remote.js
@@ -3,12 +3,12 @@
 export function install (app) {
 
   CustomVueComponent.app = app
-  import("__remote/getscript?src=alDatetimeRange/index.twigvue").then((def) => {
-    if (def.default) {
-      const com = def.default('AlDatetimeRange')
-      CustomVueComponent.register(com)
-    }
-  })
+  // import("__remote/getscript?src=alDatetimeRange/index.twigvue").then((def) => {
+  //   if (def.default) {
+  //     const com = def.default('AlDatetimeRange')
+  //     CustomVueComponent.register(com)
+  //   }
+  // })
 
   import("__remote/getscript?src=cmField/index.twigvue").then((def) => {
     if (def.default) {
