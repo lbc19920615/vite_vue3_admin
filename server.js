4ba282ce028fdbcb8aa928096597d031b071314d
baichao ling <1585638808@qq.com>
2021-12-14 13:29:07 +0800

    add serve

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+#!/usr/bin/env node
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 const { exec } = require('child_process')
-exec('npm run dev', (err, stdout, stderr) => {
+let method = argv.command ? argv.command :'gongsi'
+exec(`npm run ${method}`, (err, stdout, stderr) => {
   if (err) {
     console.error(err);
     return;
