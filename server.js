a55707cfcea6383e168547753a4893136b459359
baichao ling <1585638808@qq.com>
2021-12-17 14:27:59 +0800

    config

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+// const yargs = require('yargs/yargs')
+// const { hideBin } = require('yargs/helpers')
+// const argv = yargs(hideBin(process.argv)).argv
 const { exec } = require('child_process')
-let method = argv.command ? argv.command :'gongsi'
-exec(`npm run ${method}`, (err, stdout, stderr) => {
+let config=require('./app.json')[process.env.NODE_ENV || 'dev'];
+// console.log(process.env.NODE_ENV, config)
+
+exec(config.command, (err, stdout, stderr) => {
   if (err) {
     console.error(err);
     return;
   }
   console.log(stdout);
+  console.log('run command end', config.command)
 });
